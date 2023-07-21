22c0234983dc154c0f3b3e4a55270c1166e313e7
dragon3025 <joshuafattarini@gmail.com>
2023-07-20 21:07:14 -0700

    Crafting Golden now requires Altars

--- Global/Recipes.cs
+++ Global/Recipes.cs
@@ -70,79 +70,79 @@ namespace ReducedGrinding.GlobalRecipes
                 recipe = Recipe.Create(ItemID.GoldBird);
                 recipe.AddRecipeGroup(RecipeGroupID.Birds);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.GoldBunny);
                 recipe.AddIngredient(ItemID.Bunny);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.GoldButterfly);
                 recipe.AddRecipeGroup(RecipeGroupID.Butterflies);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.GoldDragonfly);
                 recipe.AddRecipeGroup(RecipeGroupID.Dragonflies);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.GoldFrog);
                 recipe.AddIngredient(ItemID.Frog);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.GoldGoldfish);
                 recipe.AddIngredient(ItemID.Goldfish);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.GoldGrasshopper);
                 recipe.AddIngredient(ItemID.Grasshopper);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.GoldLadyBug);
                 recipe.AddIngredient(ItemID.LadyBug);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.GoldMouse);
                 recipe.AddIngredient(ItemID.Mouse);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.GoldSeahorse);
                 recipe.AddIngredient(ItemID.Seahorse);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.SquirrelGold);
                 recipe.AddRecipeGroup(RecipeGroupID.Squirrels);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.GoldWaterStrider);
                 recipe.AddIngredient(ItemID.WaterStrider);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
 
                 recipe = Recipe.Create(ItemID.GoldWorm);
                 recipe.AddIngredient(ItemID.Worm);
                 recipe.AddIngredient(ItemID.GoldCoin, 10);
-                recipe.AddTile(TileID.CrystalBall);
+                recipe.AddTile(TileID.Altars);
                 recipe.Register();
             }
             #endregion
